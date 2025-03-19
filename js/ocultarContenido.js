ac4d2f0eb581fb12d545e1d154d09ee581aa2fec
Alejandro Bravo Calderón <alejandrobravocalderon@MacBook-Pro-de-Alejandro.local>
2025-03-19 13:40:31 +0100

    Antes de realizar el cambio de filtros de la pagina tienda

---
+++ js/ocultarContenido.js
@@ -0,0 +1,9 @@
+function entradaDatos(){
+    const selector = document.querySelector(".formulario__boton")
+    selector.addEventListener("click", (event) => {
+        elementoABorrar = document.querySelectorAll(".formulario__input")
+
+    })
+}
+
+entradaDatos()
